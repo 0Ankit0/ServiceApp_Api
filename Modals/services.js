abf2ad27cddb0280dea77f4315ecdb3eff4557ce
BenevolentButcher <ankit@insoftnepal.com>
2024-04-10 07:02:18 +0545

    description not mandatory

--- Modals/services.js
+++ Modals/services.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const serviceSchema = new mongoose.Schema({
     Name: { type: String, required: true },
-    Description: { type: String, required: true },
+    Description: { type: String },
     Price: { type: Number, required: true },
     Duration: { type: String, required: true },
 }, { timestamps: true }
