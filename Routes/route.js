65c4654c55b0b55b2e2cf15093ff9f00ce1083a1
0ankit0 <ankitpaudel.1373@gmail.com>
2024-04-19 06:53:24 +0545

    mivor changes

--- Routes/route.js
+++ Routes/route.js
@@ -2,8 +2,8 @@ import express from "express";
 import Cors from "cors";
 import userRouter from "./userRoute.js";
 import { protect } from "../Middleware/auth.js";
-import uploadRouter from "./uploadRouter.js";
-import mailRouter from "./mailRouter.js";
+import uploadRouter from "./uploadRoute.js";
+import mailRouter from "./mailRoute.js";
 import { setupChangeStream } from '../Middleware/ChangeStream.js';
 import messageRouter from "./messageRoute.js";
 
@@ -13,7 +13,8 @@ app.use(express.json());
 app.get("/", async (req, res) => {
     res.send("Welcome to the home page");
 });
-app.use('/user', setupChangeStream, userRouter);
+// app.use('/user', setupChangeStream, userRouter);
+app.use('/user', userRouter);
 app.use('/upload', protect, uploadRouter);
 app.use('/sendmain', protect, mailRouter);
 app.use('/message', protect, messageRouter);
